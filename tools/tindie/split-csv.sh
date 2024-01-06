f992081520fee5189475d3258e0a446af1d34559
TJ Horner <me@tjhorner.com>
2024-01-05 20:40:19 -0800

    Update tindie csv tool for new shipping options

--- tools/tindie/split-csv.sh
+++ tools/tindie/split-csv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm -f ud_orders.csv first_class_and_intl.csv priority.csv priority_express.csv pickup.csv
+rm -f ud_orders.csv ground_advantage_and_intl.csv priority.csv priority_express.csv pickup.csv
 
 csvq -o ud_orders.csv "SELECT * FROM all_orders WHERE \`Model Number\` LIKE \"%UPSY%\";"
 
-csvq -o first_class_and_intl.csv "SELECT * FROM ud_orders WHERE \`Shipping Method\` LIKE \"%First-Class%\" OR \`Shipping Method\` LIKE \"%Simple Export Rate%\";"
+csvq -o ground_advantage_and_intl.csv "SELECT * FROM ud_orders WHERE \`Shipping Method\` LIKE \"%Ground Advantage%\" OR \`Shipping Method\` LIKE \"%Simple Export Rate%\" OR \`Shipping Method\` LIKE \"%First-Class Package International%\";"
 csvq -o priority.csv "SELECT * FROM ud_orders WHERE \`Shipping Method\` LIKE \"%Priority Mail%\" AND \`Shipping Method\` NOT LIKE \"%Priority Mail Express%\";"
 csvq -o priority_express.csv "SELECT * FROM ud_orders WHERE \`Shipping Method\` LIKE \"%Priority Mail Express%\";"
 csvq -o pickup.csv "SELECT * FROM ud_orders WHERE \`Shipping Method\` LIKE \"%Pickup%\";"
@@ -14,7 +14,7 @@ SANITY_CHECK_QUERY=$(cat <<-END
     (SELECT count(*) from ud_orders)
       =
     (
-      (SELECT count(*) from first_class_and_intl) +
+      (SELECT count(*) from ground_advantage_and_intl) +
       (SELECT count(*) from priority) +
       (SELECT count(*) from priority_express) +
       (SELECT count(*) from pickup)
